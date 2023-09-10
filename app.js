ccee60bf16f4d7a0b7c00e4e0d7b0cd636fdd2ba
Ste11front <137266358+Ste11front@users.noreply.github.com>
2023-09-10 21:57:45 +0200

    Funzionalità

--- app.js
+++ app.js
@@ -4,8 +4,7 @@
 // - il testo scritto deve essere memorizzato nel local storage e ricaricato al ritono dell'utente
 // - conto del numero di parole e del numero di caratteri
 // - (permettere) download del txt
-// - farlo bello!!! (estetica)
-
+// - cura l'estetica
 
 // Funzionalità aggiuntive:
 
@@ -55,4 +54,69 @@ function changeTheme3(){
     console.log(mainContainer);
     mainContainer.classList.toggle('dark');
 
+}
+
+// Funzione per salvare il testo e i conteggi nel local storage
+function saveText() {
+    const div = document.getElementById('text-div');
+    localStorage.setItem('text', div.innerHTML);
+    localStorage.setItem('wordCount', document.getElementById('word-count').innerText);
+    localStorage.setItem('charCount', document.getElementById('char-count').innerText);
+}
+
+// Funzione per caricare il testo e i conteggi dal local storage
+function loadText() {
+    const div = document.getElementById('text-div');
+    const savedText = localStorage.getItem('text');
+    const savedWordCount = localStorage.getItem('wordCount');
+    const savedCharCount = localStorage.getItem('charCount');
+    if (savedText) {
+        div.innerHTML = savedText;
+    }
+    if (savedWordCount) {
+        document.getElementById('word-count').innerText = savedWordCount;
+    }
+    if (savedCharCount) {
+        document.getElementById('char-count').innerText = savedCharCount;
+    }
+}
+
+// Funzione per contare le parole e i caratteri
+function countWordsAndChars() {
+    const div = document.getElementById('text-div');
+    const text = div.innerText;
+    const words = text.split(/[\s'.,;:!?]+/).filter(function(word) { return word.length > 0; });
+    document.getElementById('word-count').innerText = 'Parole: ' + words.length;
+    document.getElementById('char-count').innerText = 'Caratteri: ' + text.length;
+}
+
+// Aggiungi un event listener per contare le parole e i caratteri ogni volta che il testo viene modificato
+document.getElementById('text-div').addEventListener('input', countWordsAndChars);
+
+// Funzione per scaricare il testo come file txt
+function downloadText() {
+    const div = document.getElementById('text-div');
+    const text = div.innerText;
+    const blob = new Blob([text], {type: 'text/plain'});
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'testo.txt';
+    a.click();
+}
+
+// Aggiungi un event listener per salvare il testo ogni volta che viene modificato
+document.getElementById('text-div').addEventListener('input', saveText);
+
+// Carica il testo quando la pagina viene caricata
+window.addEventListener('load', loadText);
+
+function toggleFullScreen() {
+    if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen();
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen(); 
+        }
+    }
 }
